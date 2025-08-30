de2a3355f85e9b2ec77fb1620c5d6ef7958185ad
Emily Flynn <emflynn@student.42london.com>
2025-08-30 18:48:13 +0100

    Add formatting files

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -1,11 +1,12 @@
-#include "WebServer.hpp"
-
 #include <cerrno>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <unistd.h>
+
+#include "WebServer.hpp"
+
 /*
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
@@ -20,8 +21,9 @@ WebServer::WebServer(const WebServer &src)
 }
 
 WebServer::WebServer(std::string ipAddress, int port)
-	: m_ipAddress(ipAddress), m_port(port), m_socket(), m_newSocket(), m_incomingMessage(),
-	  m_socketAddress(), m_socketAdddress_len(sizeof(m_socketAddress)),
+	: m_ipAddress(ipAddress), m_port(port), m_socket(), m_newSocket(),
+	  m_incomingMessage(), m_socketAddress(),
+	  m_socketAdddress_len(sizeof(m_socketAddress)),
 	  m_serverResponse(getResponse())
 {
 	m_socketAddress.sin_family = AF_INET;
@@ -30,8 +32,8 @@ WebServer::WebServer(std::string ipAddress, int port)
 
 	if (startServer() > 0)
 	{
-		std::cerr << "cannot connect to socket: " << m_port << ": " << std::strerror(errno)
-				  << std::endl;
+		std::cerr << "cannot connect to socket: " << m_port << ": "
+				  << std::strerror(errno) << std::endl;
 	}
 }
 
@@ -48,13 +50,13 @@ WebServer::~WebServer()
 ** --------------------------------- OVERLOAD ---------------------------------
 */
 
-WebServer &WebServer::operator=(WebServer const &rhs)
+WebServer &WebServer::operator=(const WebServer &rhs)
 {
 	(void)rhs;
 	return *this;
 }
 
-std::ostream &operator<<(std::ostream &o, WebServer const &i)
+std::ostream &operator<<(std::ostream &o, const WebServer &i)
 {
 	// o << "Value = " << i.getValue();
 	(void)o;
@@ -72,7 +74,7 @@ int WebServer::startServer()
 	if (m_socket < 0)
 	{
 		std::cerr << "socket() failed" << std::strerror(errno) << std::endl;
-		return (1);
+		return 1;
 	}
 	std::cout << "Socket created with value: " << m_socket << std::endl;
 
@@ -80,16 +82,18 @@ int WebServer::startServer()
 	// if you close and reopen it quickly. This tells our server that we can
 	// reuse the port.
 	int enable = 1;
-	if (setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+	if (setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) <
+	    0)
 	{
-		std::cerr << "setsockopt(SO_REUSEADDR) failed" << std::strerror(errno) << std::endl;
+		std::cerr << "setsockopt(SO_REUSEADDR) failed" << std::strerror(errno)
+				  << std::endl;
 	}
 
 	// bind socket and sever port
 	if (bind(m_socket, (sockaddr *)&m_socketAddress, m_socketAdddress_len) < 0)
 	{
 		std::cerr << "bind() failed" << std::strerror(errno) << std::endl;
-		return (1);
+		return 1;
 	}
 	std::cout << "socket bound successfully!" << std::endl;
 	return 0;
@@ -106,7 +110,8 @@ void WebServer::startListen()
 		std::cerr << "listen() failed" << std::strerror(errno) << std::endl;
 		return;
 	}
-	std::cout << "Server now listening on: " << m_ipAddress << ", port: " << m_port << std::endl;
+	std::cout << "Server now listening on: " << m_ipAddress
+			  << ", port: " << m_port << std::endl;
 	acceptConnection();
 	sendResponse();
 }
@@ -120,25 +125,34 @@ void WebServer::acceptConnection()
 	int bytes = 0;
 	char buffer[4096] = {0};
 
-	m_newSocket = accept(m_socket, (sockaddr *)&m_socketAddress, &m_socketAdddress_len);
+	m_newSocket =
+		accept(m_socket, (sockaddr *)&m_socketAddress, &m_socketAdddress_len);
 	if (m_newSocket < 0)
+	{
 		std::cerr << "accept() failed" << std::strerror(errno) << std::endl;
+	}
 	std::cout << "accepted client, newSocket fd: " << m_newSocket << std::endl;
 	bytes = recv(m_newSocket, buffer, 4096, 0);
 	if (bytes < 0)
+	{
 		std::cerr << "could not read client request" << std::endl;
+	}
 	else
 	{
-		std::cout << std::endl << "***** client request *****" << std::endl << std::endl;
+		std::cout << std::endl
+				  << "***** client request *****" << std::endl
+				  << std::endl;
 		std::cout << buffer;
-		std::cout << "***** client request over *****" << std::endl << std::endl;
+		std::cout << "***** client request over *****" << std::endl
+				  << std::endl;
 	}
 }
 
 // called at construction, creates the response message
 // confusing, needs to be broken up into multiple functions
-// ifstream sets input stream to a file, ostringstream reads the entire file using rdbuf()
-// body is the html file contents, response is some information plus the body
+// ifstream sets input stream to a file, ostringstream reads the entire file
+// using rdbuf() body is the html file contents, response is some information
+// plus the body
 std::string WebServer::getResponse()
 {
 	std::ifstream htmlFile("test.html");
@@ -146,10 +160,10 @@ std::string WebServer::getResponse()
 	std::ostringstream response;
 
 	body << htmlFile.rdbuf();
-	response << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " << body.str().size()
-			 << "\n\n"
+	response << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: "
+			 << body.str().size() << "\n\n"
 			 << body.str();
-	return (response.str());
+	return response.str();
 }
 
 // send data to client
@@ -161,15 +175,22 @@ void WebServer::sendResponse()
 	std::cout << "attempting to send response to client" << std::endl;
 	while (totalBytes < m_serverResponse.size())
 	{
-		bytesSent = send(m_newSocket, m_serverResponse.c_str(), m_serverResponse.size(), 0);
+		bytesSent = send(m_newSocket, m_serverResponse.c_str(),
+		                 m_serverResponse.size(), 0);
 		if (bytesSent < 0)
+		{
 			break;
+		}
 		totalBytes += bytesSent;
 	}
 	if (totalBytes != m_serverResponse.size())
+	{
 		std::cerr << "send() failed" << std::endl;
+	}
 	else
-	 	std::cout << "send() success!" << std::endl;
+	{
+		std::cout << "send() success!" << std::endl;
+	}
 }
 
 void WebServer::closeServer()
---
+++ src/WebServer.hpp
@@ -0,0 +1,43 @@
+#ifndef WEBSERVER_HPP
+#define WEBSERVER_HPP
+
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+
+class WebServer
+{
+public:
+	WebServer();
+	WebServer(const WebServer &src);
+	WebServer(std::string ipAddress, int port);
+	~WebServer();
+
+	WebServer &operator=(const WebServer &rhs);
+
+	int startServer();
+	void closeServer();
+
+	void startListen();
+	void acceptConnection();
+
+	std::string getResponse();
+	void sendResponse();
+
+private:
+	std::string m_ipAddress;
+	int m_port;
+	int m_socket;
+	int m_newSocket;
+	long m_incomingMessage;
+	struct sockaddr_in m_socketAddress;
+	unsigned int m_socketAdddress_len;
+	std::string m_serverResponse;
+};
+
+std::ostream &operator<<(std::ostream &o, const WebServer &i);
+
+#endif /* ******************************************************* WEBSERVER_H  \
+        */
